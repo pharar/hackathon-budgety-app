aea21b7755cc81db44aeceed8b3711a0f7a32e17
Iremaru <irene_marrero_ruiz@hotmail.com>
2018-04-09 20:37:00 +0100

    added views and partials

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+
 // const mongoose = require('mongoose');
 
 /* mongoose
@@ -9,11 +11,31 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/public', express.static('public'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/views'));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+// ============================
+// GET
+// ============================
+
+// Main route.
+app.get('/', (req, res) => {
+  console.log('Renderizando el login');
+  res.render('login');
+});
+
+// Route to the budget data.
+app.get('/app', (req, res) => {
+  console.log('Renderizando el login');
+  res.render('index');
+});
 
 app.get('/test', (req, res) => {
   res.send('It worked');
 });
+
+app.use(express.static(path.join(__dirname, '../public2')));
+app.use('/public', express.static('public'));
 
 module.exports = app;
